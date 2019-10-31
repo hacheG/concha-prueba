0a8d5fb46d694d82b39c89888dc6ef69c160a916
hacheG <943@holbertonschool.com>
2019-10-30 23:34:02 -0500

    ejecutables_test

---
+++ ce_sharp.cs
@@ -0,0 +1,11 @@
+
+using System;
+
+namespace Project_1 {
+    class MainClass {
+        public static void Main (string[] args) {
+            Console.WriteLine ("Hello World!");
+            Console.ReadKey ();
+        }
+    }
+}
---
+++ conJava.java
@@ -0,0 +1,8 @@
+#!/usr/bin/java
+public class HolaMundo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("¡Hola mundo!");
+    }
+}
